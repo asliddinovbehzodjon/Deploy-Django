6f3b2ff481b757b023802d3b2779eae96dee1148
Behzod <behzodtuit@gmail.com>
2022-11-05 21:34:20 +0500

    behzod

--- project/settings.py
+++ project/settings.py
@@ -75,10 +75,19 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'deploydjango$django',
+        'USER': 'deploydjango',
+        'PASSWORD': 'Django1234',
+        'HOST': 'deploydjango.mysql.pythonanywhere-services.com',
     }
 }
 
